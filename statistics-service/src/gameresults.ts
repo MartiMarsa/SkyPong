d4f6ff372b2c79cc8c41ee0158db71794b04acf8
MartiMarsa <126384954+MartiMarsa@users.noreply.github.com>
2026-03-04 21:52:18 +0100

    fix(statistics-service): correct stats enums import usage

--- statistics-service/src/gameresults.ts
+++ statistics-service/src/gameresults.ts
@@ -1,7 +1,9 @@
 import { getStatisticsDB } from './dbStats';
 import { getDbHelpers } from './helpers';
 import * as StatsTypes from './stats.types';
-export * as StatsEnums from './stats.enums.ts';
+import * as StatsEnums from './stats.enums';
+
+export { StatsEnums };
 
 // --- DB ---
 const db = getDbHelpers(getStatisticsDB());
