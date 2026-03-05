e05ef5ea8a93478cc0dbe932c445827845c0355f
Ilya Rozhkov <irozhkov@car12s4.42barcelona.com>
2026-03-05 21:00:43 +0100

    fix(transcendence) Fixed problem with local time and UTC

--- seed/helpers/statSeed.js
+++ seed/helpers/statSeed.js
@@ -21,7 +21,7 @@ export async function seedGames(db, users) {
         end_at,
         processed,
         game_mode
-      ) VALUES (?, ?, ?, ?, ?, ?, ?, datetime('now'), datetime('now'), 0, 'remote-pvp')`,
+      ) VALUES (?, ?, ?, ?, ?, ?, ?, datetime('now','localtime'), datetime('now','localtime'), 0, 'remote-pvp')`,
       [
         uuid(),
         a,
@@ -54,7 +54,7 @@ export async function seedGames(db, users) {
       end_at,
       processed,
       game_mode
-    ) VALUES (?, ?, ?, ?, ?, ?, ?, datetime('now'), datetime('now'), 0, 'ai')`,
+    ) VALUES (?, ?, ?, ?, ?, ?, ?, datetime('now','localtime'), datetime('now','localtime'), 0, 'ai')`,
     [
       uuid(),
       aiUserId,
