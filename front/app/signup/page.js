725a9e93b7c97582d14d0485449c572f9e3793d9
fabbbiodc <fabiodicecca@gmail.com>
2026-03-09 16:45:26 +0100

    Fixed translations

--- front/app/signup/page.js
+++ front/app/signup/page.js
@@ -55,7 +55,7 @@ export default function SignUpPage() {
         
             if (!contentType || !contentType.includes('application/json')) {
                 console.error('Response is not JSON:', await response.text());
-                setServerError(`Error del servidor. La ruta ${apiURL} no existe o está mal configurada.`);
+                setServerError(t.serverError.apiRouteError('/api/auth/signup'));
                 return;
             }
             if (!response.ok) {
