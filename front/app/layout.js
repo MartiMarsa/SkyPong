deec4b607e38d4ed25a07bcd2412d182d08d7a86
fabbbiodc <fabiodicecca@gmail.com>
2026-03-09 14:30:07 +0100

    Changed favicon

--- front/app/layout.js
+++ front/app/layout.js
@@ -20,6 +20,7 @@ export const metadata = {
   description: "Una Experiencia Trascendental de Pong",
   icons: {
     icon: [
+      { url: "/favicon.svg", type: "image/svg+xml" },
       { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
       { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
     ],
